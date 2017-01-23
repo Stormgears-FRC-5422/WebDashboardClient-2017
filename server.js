17de56e54be5c976b43ba43a9aa5e268c62b27d6
Andrew Sun <me@andrewsun.com>
2017-01-22 22:17:18 -0500

    add ability to set default state

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const DeepstreamServer = require('deepstream.io');
+const DeepstreamClient = require("deepstream.io-client-js");
+const fs = require("fs");
 const C = DeepstreamServer.constants;
 
 const server = new DeepstreamServer({
@@ -7,3 +9,10 @@ const server = new DeepstreamServer({
 });
 
 server.start();
+
+server.addListener("started", function() {
+	// Initialize default values
+	const client = DeepstreamClient("127.0.0.1:5802").login();
+	let record = client.record.getRecord("webdashboard");
+	record.set(JSON.parse(fs.readFileSync("default-data.json", "utf-8")));
+});
