532d0ed1e27911bfc69620875bbc818e884878d8
Andrew Sun <me@andrewsun.com>
2017-02-18 18:43:40 -0500

    Cleanup of copy-pasted code

--- src/lib/SyncedComponent.js
+++ src/lib/SyncedComponent.js
@@ -10,26 +10,27 @@ export default class SyncedComponent extends React.Component {
 		this._createRecord();
 		this.state = this.dsRecord.get();
 	}
-	setDeepstreamClient ( _dsClient ) {
-		dsClient = _dsClient;
-	}
+
 	componentWillMount() {
 		this._createRecord();
 	}
+
 	componentWillUnmount() {
-		setTimeout( this._destroy, 0 );
+		setTimeout(this._destroy, 0);
 	}
+
 	// componentWillUpdate( nextProps, nextState ) {
 	// 	this.dsRecord.set( this._cloneState( nextState ) );
 	// }
 	_destroy() {
-		if( this.dsRecord.isDestroyed === false ) {
-			this.dsRecord.unsubscribe( this._setState );
+		if (this.dsRecord.isDestroyed === false) {
+			this.dsRecord.unsubscribe(this._setState);
 			this.dsRecord.discard();
 		}
 
 		delete this.dsRecord;
 	}
+
 	setState = (state) => {
 		this.dsRecord.set({
 			...this.state,
@@ -37,55 +38,57 @@ export default class SyncedComponent extends React.Component {
 		});
 	};
 
-	_setState = ( state ) => {
-		console.log(state);
-		this.superSetState( this._cloneState( state ) );
+	_setState = (state) => {
+		// console.log(state);
+		this.superSetState(this._cloneState(state));
 	};
 
 	superSetState(state) {
 		super.setState(state);
 	}
 
-	_cloneState( state ) {
-		var key,
+	_cloneState(state) {
+		let key,
 			clonedState = {};
 
-		for( key in state ) {
-			if( key !== LOCAL ) {
-				clonedState[ key ] = state[ key ];
+		for (key in state) {
+			if (key !== LOCAL) {
+				clonedState[key] = state[key];
 			}
 		}
 
 		return clonedState;
 	}
+
 	_setInitialState() {
-		if( this.dsRecord && this.dsRecord.isReady && Object.keys( this.dsRecord.get() ).length === 0 && this.state ) {
-			this.dsRecord.set( this.state );
+		if (this.dsRecord && this.dsRecord.isReady && Object.keys(this.dsRecord.get()).length === 0 && this.state) {
+			this.dsRecord.set(this.state);
 		}
 	}
+
 	_createRecord() {
-		if( this.dsRecord ) {
+		if (this.dsRecord) {
 			return;
 		}
 
-		if( dsClient === null ) {
-			throw new Error( 'no deepstream client set. Please call setDeepstreamClient( ds ) before using the deepstream react mixin' );
+		if (dsClient === null) {
+			throw new Error('no deepstream client set. Please call setDeepstreamClient( ds ) before using the deepstream react mixin');
 		}
 
-		if( typeof this.props.dsRecord !== 'string' ) {
-			throw new Error( 'deepstream react mixin requires prop \'dsRecord\'' );
+		if (typeof this.props.dsRecord !== 'string') {
+			throw new Error('deepstream react mixin requires prop \'dsRecord\'');
 		}
 
-		this.dsRecord = dsClient.record.getRecord( this.props.dsRecord );
-		this.dsRecord.subscribe( this._setState );
+		this.dsRecord = dsClient.record.getRecord(this.props.dsRecord);
+		this.dsRecord.subscribe(this._setState);
 
 		/*
 		 * We can't use record.whenReady here since react complains about its internal usage of `bind`
 		 */
-		if( this.dsRecord.isReady ) {
-			setTimeout( this._setInitialState, 0 );
+		if (this.dsRecord.isReady) {
+			setTimeout(this._setInitialState, 0);
 		} else {
-			this.dsRecord.once( 'ready', this._setInitialState );
+			this.dsRecord.once('ready', this._setInitialState);
 		}
 	}
 }
