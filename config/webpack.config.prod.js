1d7304bdd2d9ceb9967cabdda0a9741f28612e66
Andrew Sun <me@andrewsun.com>
2017-02-18 20:14:10 -0500

    Fix warnings (and performance)

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -79,9 +79,9 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx', ''],
     alias: {
-      // Support React Native Web
-      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web'
+	    'react': 'inferno-compat',
+	    'react-dom': 'inferno-compat',
+	    'pure-render-decorator': paths.appSrc + "/lib/pureRenderDecorator",
     }
   },
   
