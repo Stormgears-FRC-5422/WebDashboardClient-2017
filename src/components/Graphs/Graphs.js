bb3264b780af79a6e295188f7120c241a7c693bd
Andrew Sun <me@andrewsun.com>
2017-02-20 12:15:33 -0500

    Start working on graphs

---
+++ src/components/Graphs/Graphs.js
@@ -0,0 +1,15 @@
+import Inferno from "inferno";
+import Component from "inferno-component";
+import Draggable from "react-draggable";
+
+export default class Graphs extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return <div></div>
+	}
+}
