d354e7cdd655d189089a9723dec73e1115e7184a
Andrew Sun <me@andrewsun.com>
2017-04-08 20:18:35 -0400

    Upgrade Node.js in Windows install script

--- scripts/windows-install.bat
+++ scripts/windows-install.bat
@@ -1,7 +1,7 @@
-"windows-tools\wget.exe" --no-check-certificate https://nodejs.org/dist/v7.7.4/node-v7.7.4-win-x64.zip
-"windows-tools\7za.exe" x "node-v7.7.4-win-x64.zip" -r > nul
-del node-v7.7.4-win-x64.zip
-move /Y node-v7.7.4-win-x64 node
+"windows-tools\wget.exe" --no-check-certificate https://nodejs.org/dist/v7.8.0/node-v7.8.0-win-x64.zip
+"windows-tools\7za.exe" x "node-v7.8.0-win-x64.zip" -r > nul
+del node-v7.8.0-win-x64.zip
+move /Y node-v7.8.0-win-x64 node
 
 set NODE_ENV=production
 node/npm --scripts-prepend-node-path=true install
