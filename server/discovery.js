7809bab12c74c73df2e936647dde1ca53b66cfa7
Andrew Sun <me@andrewsun.com>
2017-04-09 21:40:07 -0400

    Multicast discovery

--- server/discovery.js
+++ server/discovery.js
@@ -1,15 +1,51 @@
 const dgram = require("dgram");
-const server = dgram.createSocket("udp4");
+const server = dgram.createSocket({type: "udp4", reuseAddr: true});
 
+server.on('listening', function () {
+	server.setBroadcast(true);
+	server.setMulticastTTL(128);
+
+	server.addMembership("224.0.0.251", "0.0.0.0");
+
+	const address = server.address();
+	console.log('Multicast discovery server listening on ' + address.address + ":" + address.port);
+});
+server.on('message', function (message, remote) {
+	const address = remote.address;
+	const msg = message.toString();
+
+
+	if (msg === "WEBDASHBOARD_REQUEST") {
+		console.log("Got a multicast request packet from " + address);
+		server.send("WEBDASHBOARD_DISCO", 5802, address);
+	}
+});
+server.bind(5353, "0.0.0.0");
+
+const unicastServer = dgram.createSocket({type: "udp4", reuseAddr: true});
+unicastServer.on("listening", function () {
+	const address = unicastServer.address();
+	console.log('Unicast discovery server listening on ' + address.address + ":" + address.port);
+});
+unicastServer.on('message', function (message, remote) {
+	const address = remote.address;
+	const msg = message.toString();
+
+	console.log("Got a unicast request packet from " + address);
+
+	if (msg === "WEBDASHBOARD_REQUEST") {
+		server.send("WEBDASHBOARD_DISCO", 5802, address);
+	}
+});
+unicastServer.bind(5803, "0.0.0.0");
+
+// Fallback for when multicast isn't working
 const possible = [
 	"10.54.22.2",
-	"10.54.22.20",
-	"127.0.0.1"
+	"10.54.22.20"
 ];
-
-setInterval(function() {
-	possible.forEach(function(addr) {
-		// server.send("WEBDASHqerlkguaaiuehbadjxygaeisuwdbvkqhi357oreahfdbsklarszyso7hw4erlsukdfhcvx jbwemhsvdcxjyfjhwerdfisuhjvdfsBOARD_DISCO", 5802, addr);
+setInterval(function () {
+	possible.forEach(function (addr) {
 		server.send("WEBDASHBOARD_DISCO", 5802, addr);
 	});
-}, 500);
+}, 3000);
