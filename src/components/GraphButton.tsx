764f92fed9a04a6340437db33086e12659557091
Andrew Sun <me@andrewsun.com>
2017-04-14 17:56:13 -0400

    Fix a lot of issues

--- src/components/GraphButton.tsx
+++ src/components/GraphButton.tsx
@@ -1,9 +1,14 @@
-function handleClick(props) {
+export interface GraphButtonProps {
+	disabled: boolean;
+	path: string;
+}
+
+function handleClick(path: string) {
 	return function() {
-		global["Graphs"].addGraph(props.path);
+		global["Graphs"].addGraph(path);
 	};
 }
 
-export default function GraphButton(props) {
+export default function GraphButton(props: GraphButtonProps) {
 	return <button className={"pt-button pt-minimal pt-icon-timeline-line-chart" + (props.disabled ? " pt-disabled" : "")} onClick={props.disabled ? null : handleClick(props.path)}/>
 }
