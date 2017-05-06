ea86594655223e57aacf97ef1b778d200cf6b17a
Andrew Sun <me@andrewsun.com>
2017-05-05 23:02:18 -0400

    Fix issue with static file server

--- server/static.js
+++ server/static.js
@@ -1,8 +1,11 @@
+const path = require("path");
 const superstatic = require("superstatic").server;
+
 const app = superstatic({
 	port: 5803,
 	host: "0.0.0.0",
-	gzip: true
+	gzip: true,
+	cwd: path.join(__dirname, "../build")
 });
 
 const server = app.listen(function() {
